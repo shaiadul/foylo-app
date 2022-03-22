eeec809847d33475cb49fc375dbab9f1f442154e
shaiadul <devshaiadul69@gmail.com>
2022-03-23 00:27:04 +0600

    total price, total shipping , tax added in stiky container

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import './Cart.css';
 const Cart = ({cart}) => {
+
+    // total
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = (total * 0.1).toFixed(2);
+    // const tax = total * 10 /100;
+
     return (
-    <div>
+    <div className='cart'>
         <h4>this is order summery</h4>
         <p>Selected Itmes: {cart.length}</p>
+        <p>Total Price: ${total}</p>
+        <p>Total Shipping: ${shipping}</p>
+        <p>Tax: ${tax}</p>
+        <h5>Grand Total: </h5>
+
     </div>
     );
 };
